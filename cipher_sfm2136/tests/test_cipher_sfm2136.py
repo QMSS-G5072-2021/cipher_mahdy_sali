b8e6fa77ecfefd3d205cac71bf5097e70a0da466
Sali <sali.mahdy@gmail.com>
2021-11-07 17:43:54 -0500

    Added Tests

--- cipher_sfm2136/tests/test_cipher_sfm2136.py
+++ cipher_sfm2136/tests/test_cipher_sfm2136.py
@@ -1,1 +1,15 @@
 from cipher_sfm2136 import cipher_sfm2136
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+def test_cipher_shift():
+    assert cipher('apple',-1)=='Zookd'
